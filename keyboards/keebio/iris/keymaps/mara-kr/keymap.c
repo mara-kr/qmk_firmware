eefe624ca94494248b3db6e266aca1189e697ee1
Mara Kirdani-Ryan <me@marakr.com>
2021-10-16 10:10:11 -0700

    Close tap on encoder press

--- keyboards/keebio/iris/keymaps/mara-kr/keymap.c
+++ keyboards/keebio/iris/keymaps/mara-kr/keymap.c
@@ -1,4 +1,3 @@
-// TODO: What does tapping the encoder do while scrolling?
 // TODO: Better lighting editing! Trans pride pattern moving across the board?
 #include QMK_KEYBOARD_H
 
@@ -59,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      LT_ESC,   KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                              KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_Z,    KC_X,       KC_C,   KC_D,   KC_V,    KC_B,            LT_SPCE,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+     KC_LSFT, KC_Z,    KC_X,       KC_C,   KC_D,   KC_V,   G(KC_W),            LT_SPCE,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_LGUI,  MO_NUM,  KC_BSPC,                  LT_SPCE, KC_LCTL,  LAUNCH
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
